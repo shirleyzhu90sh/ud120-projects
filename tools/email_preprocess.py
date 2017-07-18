9ec1c6aa390af7475eb4fe5c4df440b9c07f257e
shirley.zhu <zsq19900619@126.com>
2017-07-18 19:06:24 +0800

    cross_validation module would be dropped out, use sklearn.model_selection.train_test_split instead.

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -3,8 +3,9 @@
 import pickle
 import cPickle
 import numpy
+import sklearn
 
-from sklearn import cross_validation
+#from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -39,7 +40,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = sklearn.model_selection.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
