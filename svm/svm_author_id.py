1564cc879da0f3ca7b447be123cb4795c09d730c
shirley.zhu <zsq19900619@126.com>
2017-07-19 08:36:20 +0800

    Build classification using svm，

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,43 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+
+#clf = svm.SVC(kernel = 'linear') #accuracy: 0.884527872582 for 1% data
+clf = svm.SVC(C = 10000.0, kernel = 'rbf') 
+#C = 10.0 accuracy: 0.616040955631 for 1% data
+#C = 100.0 accuracy: 0.616040955631 for 1% data
+#C = 1000.0 accuracy: 0.821387940842 for 1% data
+#C = 10000.0 accuracy: 0.892491467577 for 1% data, 0.990898748578 for 100% data
+
+
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "fitting time:", round(time()-t0, 3), "s"
+
+
+tp = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-tp, 3), "s"
+
+print "answer_10:", pred[10]
+print "answer_26:", pred[26]
+print "answer_50:", pred[50]
+
+count = 0
+for i in range(len(features_test)):
+    if pred[i] == 1:
+        count = count + 1
+
+print "Number of Chris(1) class:", count
+
+accuracy = clf.score(features_test, labels_test)
+
+print "accuracy:", accuracy
 
 #########################################################
 
